7b1873c2c0ad99d6d0733d9b455bbd1b381e6097
Junaid Maqsood <junaidmaqsood@live.com>
2021-05-18 12:31:31 +0500

    Brand logo minor fix

--- resources/views/dashboard/layout/css.blade.php
+++ resources/views/dashboard/layout/css.blade.php
@@ -8,4 +8,9 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('/css/themes/vertical-menu.min.css') }}">
 <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 <link rel="stylesheet" type="text/css" href="/css/pages/sweetalert2.min.css">
+<style>
+    .main-menu .navbar-header .navbar-brand .brand-logo{
+        background-size: contain !important;
+    }
+</style>
 @stack('css')
