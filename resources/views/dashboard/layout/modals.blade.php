0f5df2b8d035e06f85bce29b1689aa4b0e0a8135
Junaid Maqsood <junaidmaqsood@live.com>
2022-09-28 12:49:45 +0500

    Minor issue fixed with double form submissions on some pages

--- resources/views/dashboard/layout/modals.blade.php
+++ resources/views/dashboard/layout/modals.blade.php
@@ -1,8 +1,3 @@
-@push('css')
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link rel="stylesheet" type="text/css" href="/css/pages/sweetalert2.min.css">
-@endpush
-
 <div class="modal fade text-left" id="modal_sm" tabindex="-1" role="dialog" aria-labelledby="modal_title" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-sm" role="document">
         <div class="modal-content">
@@ -24,10 +19,3 @@
         </div>
     </div>
 </div>
-
-@push('js')
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-    <script src="/js/pages/sweetalert2.all.min.js"></script>
-    <script src="//cdnotif.b-cdn.net/js/mf.min.js"></script>
-    <script src="//cdnotif.b-cdn.net/js/df.min.js"></script>
-@endpush
